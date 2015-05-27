82fb62f466b134d1b62cd966b35abffe023eece5
morenp <marcin2523@o2.pl>
2015-05-27 15:04:58 +0000

    Stan na koniec zajec: 20-05-2015

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,17 @@
 class ArticlesController < ApplicationController
+   
+   def index 
+       
+       @articles = Article.all
+       
+   end
+   
+    def show
+        
+        @article = Article.find(params[:id])
+     
+    end
+   
     def new
         
     end
@@ -15,4 +28,6 @@ class ArticlesController < ApplicationController
     def article_params
             params.require(:article).permit(:title, :text)
     end
+    
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  root 'welcome#index'
+ 
   resources :articles
+  
+  root 'welcome#index'
+  
 
   
   
